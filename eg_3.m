ea6bb88f3a5ec10ff4c0793eeb2720397d420468
YanLuLin <163007489+YanLuLin@users.noreply.github.com>
2024-03-12 00:05:02 +0800

    Add files via upload

---
+++ eg_3.m
@@ -0,0 +1,7 @@
+function [A,b,x_star] = eg_3(m,mu)
+n=m^2;
+S=sparse(4*speye(m)-1.5*diag(ones(m-1,1),-1)-0.5*diag(ones(m-1,1),1));
+A_hat=kron(-1.5*diag(ones(1,m-1),-1)-0.5*diag(ones(1,m-1),1),speye(m))+kron(diag(ones(1,m)),S);  
+A=sparse(A_hat+mu*speye(n));
+x_star=zeros(n,1)+(-1).^(1:n)'; 
+b=A*x_star-abs(x_star);
